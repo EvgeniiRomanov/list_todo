ca49fd98e35b079317db0251417c9627ed28c086
EvgeniiRomanov <e78rus@mail.ru>
2022-05-31 15:10:07 +0000

    1.создана модель 2.подключена и настроена админка 3.создан и пройден тест на создание заметки в БД

--- todo_api/test/test_models.py
+++ todo_api/test/test_models.py
@@ -1,18 +1,24 @@
 import unittest
 
-from rest_framework.test import TestCase
+from django.test import TestCase
 from rest_framework import status
-from .models
+from todo_api.models import Note
 from django.contrib.auth.models import User
 
 
 class ModelTestCase(TestCase):
-    def setUp(self):
-        self.task_name = "Выполнить лабораторную работу №9"
-        self.task = Task(name=self.task_name)
+    """ Тест на создание заметки в БД"""
+    @classmethod
+    def setUpTestData(cls):                              # создаем пользователя
+        User.objects.create(username='test1@test.ru')    # пользователь по умолч. для всех тестов
 
-    def test_model_can_create_task(self):
-        old_count = Task.objects.count()
-        self.task.save()
-        new_count = Task.objects.count()
+    def setUp(self):    # создаем одну заметку
+        self.task_name = "Создание тестовой заметки"
+        test_user = User.objects.get(username='test1@test.ru')          # достаем пользователя
+        self.task = Note(nt_title=self.task_name, nt_author=test_user)  # создаем заметку с необх. полями
+
+    def test_model_can_create_Note(self):
+        old_count = Note.objects.count()        # считаем число заметок в БД
+        self.task.save()                        # помещаем созданную в def SetUp заметку в БД
+        new_count = Note.objects.count()
         self.assertNotEqual(old_count, new_count)
